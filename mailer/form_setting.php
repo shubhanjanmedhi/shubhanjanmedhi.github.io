c2beed9126d1fe2278ae73483b594291d772abba
DESKTOP-0VGNJDV\Asus <s.medhi1@gmail.com>
2019-05-11 21:47:38 +0530

    Update form_setting.php

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$subj = "New message from the site 'https://shubhanjanmedhi.github.io/'"; //letter subject
+	$to = 's.medhi1@gmail.com'; // Enter Your E-mail
+	$from = 's.medhi1@gmail.com'; // Admin e-mail
+	$fromName = 'Github.io'; // Your company name
 	$charset = 'UTF-8';
 ?>
