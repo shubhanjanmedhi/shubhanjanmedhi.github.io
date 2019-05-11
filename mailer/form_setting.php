f15b1cc2cc03aac7c316ad2892a2a2f96af069e5
DESKTOP-0VGNJDV\Asus <s.medhi1@gmail.com>
2019-05-11 21:50:40 +0530

    updated

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'https://shubhanjanmedhi.github.io/'"; //letter subject
-	$to = 's.medhi1@gmail.com'; // Enter Your E-mail
-	$from = 's.medhi1@gmail.com'; // Admin e-mail
-	$fromName = 'Github.io'; // Your company name
-	$charset = 'UTF-8';
-?>
